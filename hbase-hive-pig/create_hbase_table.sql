8bacbdb82826bca3981826d94ff48b7fe7f5bea7
Romain Rigaux <romain.rigaux@gmail.com>
2013-09-04 14:13:09 -0700

    Update create_hbase_table.sql

--- hbase-hive-pig/create_hbase_table.sql
+++ hbase-hive-pig/create_hbase_table.sql
@@ -1,3 +1,6 @@
+
+-- Create table in Hive/HBase
+
 set hbase.zookeeper.quorum localhost
 
 CREATE TABLE top_cool_hbase (key string, value map<string, int>)
@@ -7,9 +10,7 @@ TBLPROPERTIES ("hbase.table.name" = "top_cool");
 
 
 
-
-
-
+-- Insert data
 
 ADD JAR /usr/lib/hive/lib/zookeeper.jar;
 ADD JAR /usr/lib/hive/lib/hbase.jar;
@@ -17,4 +18,9 @@ ADD JAR /usr/lib/hive/lib/hive-hbase-handler-0.10.0-cdh4.3.0.jar
 ADD JAR /usr/lib/hive/lib/guava-11.0.2.jar;
 
 INSERT OVERWRITE TABLE top_cool_hbase SELECT name, map(`date`, cast(coolness as int)) FROM top_cool
+
+
+-- Insert cooler data: restaurant names
+
+INSERT OVERWRITE TABLE top_cool_hbase SELECT name, map(`date`, cast(r.stars as int)) FROM review r join  business b on r.business_id = b.business_id;
 
