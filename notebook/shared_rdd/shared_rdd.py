87ccac367227d7f6efd9827c7f65a38bdcf37eda
Romain Rigaux <romain.rigaux@gmail.com>
2015-10-28 00:16:36 +0100

    Update indentation

--- notebook/shared_rdd/shared_rdd.py
+++ notebook/shared_rdd/shared_rdd.py
@@ -4,15 +4,12 @@
 # To type in a regular Python shell.
 # Depends on: pip install requests
 # 
-
 import requests
 import json
 
 
-class SharedRdd():
-  """
-  Perform REST calls to a remote PySpark shell containing a Shared named RDD.
-  """  
+class SharedRdd2():
+  
   def __init__(self, session_url, name):
     self.session_url = session_url
     self.name = name
@@ -26,22 +23,22 @@ class SharedRdd():
   def _curl(self, code):
     statements_url = self.session_url + '/statements'
     data = {'code': code}
+    print data
+    print statements_url
     r = requests.post(statements_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
     resp = r.json()
     statement_id = str(resp['id'])
     while resp['state'] == 'running':
       r = requests.get(statements_url + '/' + statement_id)
-      resp = r.json()  
-
+      resp = r.json()
     if 'output' in resp: # Case Livy returns automatically
       return resp['output']['data']
     else:
       return resp['data']
+ 
+
+states = SharedRdd2('http://localhost:8998/sessions/0', 'states')
 
 
-states = SharedRdd('http://localhost:8998/sessions/0', 'states')
-
-
-print states.get('ak')
-print states.get('ca')
-
+states.get('ak')
+states.get('ca')
